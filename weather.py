3481ba0287182ea824a3b1c18d145f09ef71cde8
Dmitriy877 <dmitriybukin05@gmail.com>
2024-12-17 21:36:05 +0300

    Solve mistakes

--- weather.py
+++ weather.py
@@ -5,10 +5,12 @@ def main():
 
 	places = ["Лондон", "svo", "Череповец"]
 
+	payload = {"MnTqu":"", "lang":"ru"}
+
 	for place in places:
 
-		url_weather = 'https://wttr.in/{0}?MnTqu&lang=ru'.format(place)
-		response = requests.get(url_weather)
+		weather_url = 'https://wttr.in/{0}?MnTqu&lang=ru'.format(place)
+		response = requests.get(weather_url, params=payload)
 		response.raise_for_status()
 		print(response.text)
 
